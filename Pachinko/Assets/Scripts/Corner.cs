db87edda91fafa78e4a24d2f1668c21671aa4be2
jexpop <jexpouoc@gmail.com>
2021-09-05 14:04:23 +0200

    Pachinko update

---
+++ Pachinko/Assets/Scripts/Corner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Corner : MonoBehaviour
+{
+    public Text CounterText;
+
+    private int Count = 0;
+
+    private void Start()
+    {
+        Count = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Count += 1;
+        CounterText.text = CounterText.name +  " Count : " + Count;
+    }
+}
